bcfba4d68c7824558dfdf45416f28d87fea5302b
Mystic5523 <Mystic5523@hotmail.com>
2018-10-15 19:37:24 -0500

    added spell seeds

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <Link to="/">Home</Link>
+    <Link to="/">Spellbook</Link>
 
     <nav>
       <Link to="/helloworld">Hello World</Link>
