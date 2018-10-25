1eed9acb7e0bf376541453faf256d4436de9ce2e
Mystic5523 <Mystic5523@hotmail.com>
2018-10-24 21:34:27 -0500

    more ugly

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -13,14 +13,8 @@ module.exports = merge(commonConfig, {
   },
 
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true
-      },
-      output: {
-        comments: false
-      }
+    new UglifyJsPlugin({
+      sourceMap: true
     })
   ]
 });
